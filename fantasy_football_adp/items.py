b34a0980ae84000c772d1450060ad8b3a3ec7793
Jason Miguel <jason.miguel@gmail.com>
2020-09-24 12:53:27 -0400

    added items

--- fantasy_football_adp/items.py
+++ fantasy_football_adp/items.py
@@ -7,6 +7,26 @@ import scrapy
 
 
 class FantasyFootballAdpItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    position = scrapy.Field()
+    age = scrapy.Field()
+    games = scrapy.Field()
+    games_started = scrapy.Field()
+    pass_completed = scrapy.Field()
+    pass_attempted = scrapy.Field()
+    pass_yards = scrapy.Field()
+    pass_tds = scrapy.Field()
+    int = scrapy.Field()
+    rush_att = scrapy.Field()
+    rush_yds = scrapy.Field()
+    yds_per_att = scrapy.Field()
+    rush_tds = scrapy.Field()
+    pass_targets = scrapy.Field()
+    rec = scrapy.Field()
+    rec_yards = scrapy.Field()
+    yards_per_rec = scrapy.Field()
+    rec_tds = scrapy.Field()
+    fumbles = scrapy.Field()
+    fumbles_lost = scrapy.Field()
+    total_tds = scrapy.Field()
+    two_pt_conv_made = scrapy.Field()
+    two_pt_conv_pass = scrapy.Field()
